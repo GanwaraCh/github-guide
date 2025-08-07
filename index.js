acc8b565787d6ee2c4d0f66e01a7f92bc05a83ad
GanwaraDegile <ganwara.chalakklang@digile.net>
2025-08-07 18:10:21 +0700

    add subtract

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function sum(a, b) {
   return a + b;
 }
 console.log(sum(5, 3));
+function subtract(...nums) {
+  if (nums.length === 0) return 0;
+  return nums.reduce((a, b) => a - b);
+}
+console.log(subtract(100, 20, 10));
