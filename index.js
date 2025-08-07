22e7fc463ed48c57da3249877a8a9e986726e497
GanwaraDegile <ganwara.chalakklang@digile.net>
2025-08-07 17:30:51 +0700

    Revert "add function divide"
    This reverts commit 0d7ede06b87d3428a25511fc4d3e1867f543bda8.

--- index.js
+++ index.js
@@ -9,10 +9,4 @@ console.log(subtract(10, 3));
 function multiply(a, b) {
   return a * b;
 }
-console.log(multiply(5, 3)); function divide(a, b) {
-  if (b === 0) {
-    return 'Error: หารด้วยศูนย์ไม่ได้';
-  }
-  return a / b;
-}
-console.log(divide(10, 2));
+console.log(multiply(5, 3));
